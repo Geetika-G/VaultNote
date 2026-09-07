71976b7b1226592abc1b1d093decc7ca482ccee6
Geetika-G <g.geetika0911@gmail.com>
2026-09-07 12:49:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,4 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(err)
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+module.exports = app
